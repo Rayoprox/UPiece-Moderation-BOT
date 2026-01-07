fc98a3212747fee512a2ca9eb835c5fc90a986e2
LuissGA <lg.nunes.2024@alumnos.urjc.es>
2026-01-07 16:05:52 +0100

    Fix Token

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,6 +2,7 @@ const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
 require('dotenv').config();
+console.log("Token:", process.env.DISCORD_TOKEN ? "SÍ" : "NO");
 
 const mainGuildCommands = [];
 const appealGuildCommands = [];
