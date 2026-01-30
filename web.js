c33afc4ebf9ee6f0bff838f7a8bf2f7290b4ccfb
LuissGA <lg.nunes.2024@alumnos.urjc.es>
2026-01-30 19:33:03 +0100

    Fix cookies

--- web.js
+++ web.js
@@ -33,10 +33,15 @@ app.set('views', join(__dirname, 'views'));
 app.use(express.static(join(__dirname, 'public')));
 app.use(express.json());
 app.use(session({
-    secret: process.env.SESSION_SECRET || 'dev_secret',
+
+    name: `session_${process.env.CLIENT_ID.slice(-5)}`, 
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false }
+    cookie: {
+        secure: true, 
+        httpOnly: true
+    }
 }));
 app.use(passport.initialize());
 app.use(passport.session());
